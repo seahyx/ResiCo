5c32699fbac801c6ff812882437e416f99899afc
Seah Ying Xiang <seahyx@gmail.com>
2023-03-28 03:04:26 +0800

    Squashed commit of the following:
    commit dfc592bb2a6f3dc335203752bc91b546549375e2
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Tue Mar 28 03:02:43 2023 +0800
    
        Added top bar for home page
    
    commit 689a791c9df11b75ac70487343d952a0886d3155
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Mon Mar 27 22:39:34 2023 +0800
    
        Updated home and forum icons
    
    commit 8abd0b149cec301c474d061418e867b8f8ad1415
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Mon Mar 27 22:37:46 2023 +0800
    
        Added bottom navigation bar

--- app/src/main/java/com/example/resico/MainActivity.java
+++ app/src/main/java/com/example/resico/MainActivity.java
@@ -1,28 +1,36 @@
 package com.example.resico;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.resico.databinding.ActivityMainBinding;
+import com.google.android.material.behavior.HideBottomViewOnScrollBehavior;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 	private AppBarConfiguration appBarConfiguration;
 	private ActivityMainBinding binding;
+	private BottomNavigationView bottomNavView;
+	private FragmentContainerView navHostView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +39,20 @@ public class MainActivity extends AppCompatActivity {
 		binding = ActivityMainBinding.inflate(getLayoutInflater());
 		setContentView(binding.getRoot());
 
-//		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//		appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+		bottomNavView = binding.bottomNav;
+		navHostView = binding.contentMain.navHostFragmentContentMain;
+
+		NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
+		NavController navController = navHostFragment != null ? navHostFragment.getNavController() : null;
+		if (navController != null) {
+			navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> toggleBottomNavBehavior(false));
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
+		getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
 		return true;
 	}
 
@@ -51,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
 
 		return super.onOptionsItemSelected(item);
 	}
@@ -63,5 +76,19 @@ public class MainActivity extends AppCompatActivity {
 		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
 		return NavigationUI.navigateUp(navController, appBarConfiguration)
 				|| super.onSupportNavigateUp();
+	}
+
+	private void  toggleBottomNavBehavior(boolean scrollToHide) {
+		// Get the layout params of the bottom nav
+		CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) bottomNavView.getLayoutParams();
+		params.setBehavior(scrollToHide ? new HideBottomViewOnScrollBehavior<CoordinatorLayout>(this, null) : null);
+
+		// Add margin to Nav Host fragment if the layout behaviour is not added
+		int id = getResources().getIdentifier(
+				getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ? "navigation_bar_height" : "navigation_bar_height_landscape",
+				"dimen", "android");
+		((ConstraintLayout.LayoutParams) navHostView.getLayoutParams()).setMargins(
+				0, 0, 0, scrollToHide ? 0 : getResources().getDimensionPixelSize(id)
+		);
 	}
 }
