e59d9362dbe66e10a76f8a0dcade14a84fb024bf
Seah Ying Xiang <seahyx@gmail.com>
2023-03-31 20:38:58 +0800

    Squashed commit of the following:
    commit 8d3d70c9f1b4216013c924b5cc6a71e83418f4c2
    Merge: edc59ff c7c2ecd
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Fri Mar 31 20:38:06 2023 +0800
    
        Merge branch 'feature-events'
    
    commit edc59ff8e486119dfcf39177ee618b02cf751166
    Author: jsonggan <1006283@mymail.sutd.edu.sg>
    Date:   Fri Mar 31 20:20:10 2023 +0800
    
        adapter file
    
    commit 77d52cc025e614412d3648a83c28aa3b818890cd
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Fri Mar 31 20:08:50 2023 +0800
    
        Squashed commit of the following:
    
        commit 37290410e8885a41f11353b1bd416f80d23057cd
        Author: Seah Ying Xiang <seahyx@gmail.com>
        Date:   Fri Mar 31 20:06:49 2023 +0800
    
            Updated placeholder profile
    
        commit 95a549deba7c971fa1cabc1814ce30d2098d48ba
        Author: Seah Ying Xiang <seahyx@gmail.com>
        Date:   Fri Mar 31 04:06:59 2023 +0800
    
            Linked event list to API
    
        commit e51f5211b24008e34c4c3726f02be3f2d50bfc0f
        Author: Seah Ying Xiang <seahyx@gmail.com>
        Date:   Fri Mar 31 04:06:00 2023 +0800
    
            Fixed events RecyclerView not respecting match_parent in child
    
    commit d0a25ea530ad75520179d56b39071e9fccd8f8d3
    Author: jsonggan <1006283@mymail.sutd.edu.sg>
    Date:   Fri Mar 31 19:24:34 2023 +0800
    
        layout for forum detail
    
    commit 8ded5ca5e0c09c716269774b59a356dce97c60aa
    Author: Seah Ying Xiang <seahyx@gmail.com>
    Date:   Fri Mar 31 13:22:44 2023 +0800
    
        Squashed commit of the following:
    
        commit 95a549deba7c971fa1cabc1814ce30d2098d48ba
        Author: Seah Ying Xiang <seahyx@gmail.com>
        Date:   Fri Mar 31 04:06:59 2023 +0800
    
            Linked event list to API
    
        commit e51f5211b24008e34c4c3726f02be3f2d50bfc0f
        Author: Seah Ying Xiang <seahyx@gmail.com>
        Date:   Fri Mar 31 04:06:00 2023 +0800
    
            Fixed events RecyclerView not respecting match_parent in child

--- app/src/main/java/com/example/resico/ForumsFragment.java
+++ app/src/main/java/com/example/resico/ForumsFragment.java
@@ -2,15 +2,22 @@ package com.example.resico;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.resico.databinding.FragmentEventsBinding;
+import com.example.resico.databinding.FragmentForumsBinding;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ForumsFragment#newInstance} factory method to
+ * Use the {@link ForumsFragment newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ForumsFragment extends Fragment {
@@ -23,42 +30,31 @@ public class ForumsFragment extends Fragment {
 	// TODO: Rename and change types of parameters
 	private String mParam1;
 	private String mParam2;
+	private FragmentForumsBinding binding;
 
 	public ForumsFragment() {
 		// Required empty public constructor
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment ForumsFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static ForumsFragment newInstance(String param1, String param2) {
-		ForumsFragment fragment = new ForumsFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
-		return fragment;
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
-	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_forums, container, false);
+		binding = FragmentForumsBinding.inflate(inflater,container,false);
+		return binding.getRoot();
 	}
+
+	public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		binding.buttonToForum.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				@NonNull NavDirections action = ForumsFragmentDirections.actionNavForumsToNavForumsDetail();
+				NavHostFragment.findNavController(ForumsFragment.this).navigate(action);
+			}
+		});
+	}
+
 }
